e979f2e6bc2f0b16aec7009e53379fb14d5a6024
Leonid <belyaevleonid2000@mail.ru>
2021-06-22 00:44:18 +0300

    ГИД)))

--- js/carousel.js
+++ js/carousel.js
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded', () => {
     let pictures = document.getElementsByClassName('img')
     const pic_count = pictures.length
     let iter = pic_count - 1;
+    
+    let line = document.getElementById('line');
+
+    if (map) {
+        line.style.visibility = 'hidden';
+    }
 
     left.addEventListener('mouseover', () => {
         leftarrow.style.opacity = '100%';
@@ -18,8 +24,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     left.addEventListener('click', () => {
         pictures[iter % pic_count].style.visibility = 'hidden';
-        iter = --iter < 0 ? pic_count - 1 : iter;
-        pictures[iter % pic_count].style.visibility = 'visible';
+        pictures[++iter % pic_count].style.visibility = 'visible';
+        if (map) {
+            if (iter == pic_count - 1 || iter == 0 || iter == pic_count) {
+                line.style.visibility = 'hidden';
+            } else {
+                line.style.visibility = 'visible';
+            }
+        }
     });
 
 
@@ -32,7 +44,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     right.addEventListener('click', () => {
         pictures[iter % pic_count].style.visibility = 'hidden';
-        pictures[++iter % pic_count].style.visibility = 'visible';
+        iter = --iter < 0 ? pic_count - 1 : iter;
+        pictures[iter % pic_count].style.visibility = 'visible';
+        if (map) {
+            if (iter == pic_count - 1 || iter == 0) {
+                line.style.visibility = 'hidden';
+            } else {
+                line.style.visibility = 'visible';
+            }
+        }
     });
 
 });
