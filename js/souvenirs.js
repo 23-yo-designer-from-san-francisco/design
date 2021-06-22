26a9d7c18b33d119e6af7fdcd30037168aa48370
Leonid <belyeavleonid2000@mail.ru>
2021-06-22 22:18:38 +0300

    Souvenirs ready

--- js/souvenirs.js
+++ js/souvenirs.js
@@ -1,5 +1,7 @@
 let shadow = document.getElementById('shadow');
 
+let last_highlighted = null;
+
 function buy(id) {
     id = id > 3 ? id - 1 : id;
     shadow.style.visibility = 'visible';
@@ -7,6 +9,17 @@ function buy(id) {
 }
 
 function close_modal(e) {
+    last_highlighted.style.visibility = 'hidden';
     e.parentElement.style.visibility = 'hidden';
     shadow.style.visibility = 'hidden';
+}
+
+function hover(e, yes, direction) {
+    if (yes) {
+        last_highlighted = e.parentElement.getElementsByClassName('rectangle-92')[0];
+        last_highlighted.style.visibility = direction ? 'visible' : 'hidden';
+    } else {
+        last_highlighted = e.parentElement.getElementsByClassName('rectangle-93')[0];
+        last_highlighted.style.visibility = direction ? 'visible' : 'hidden';
+    }
 }
