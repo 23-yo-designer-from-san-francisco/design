ea22a38a462cc135b4b1b5bc6eac4545bf9eeef1
Leonid <belyaevleonid2000@mail.ru>
2021-06-22 18:16:05 +0300

    Added souvenirs

--- js/souvenirs.js
+++ js/souvenirs.js
@@ -5,3 +5,8 @@ function buy(id) {
     shadow.style.visibility = 'visible';
     document.getElementById(`buy${id}`).style.visibility = 'visible';
 }
+
+function close_modal(e) {
+    e.parentElement.style.visibility = 'hidden';
+    shadow.style.visibility = 'hidden';
+}
